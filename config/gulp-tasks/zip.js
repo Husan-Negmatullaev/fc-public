580de25ad7ba26e55b2037bc4f8b42bb430ef389
Husan-Negmatullaev <84503199+Husan-Negmatullaev@users.noreply.github.com>
2023-02-17 13:37:38 +0500

    Update zip.js

--- config/gulp-tasks/zip.js
+++ config/gulp-tasks/zip.js
@@ -1,15 +1,15 @@
-import del from "del";
+import { deleteAsync } from 'del';
 import zipPlugin from "gulp-zip";
 
-export const zip = () => {
-	del(`./${app.path.rootFolder}.zip`);
-	return app.gulp.src(`${app.path.buildFolder}/**/*.*`, {})
-		.pipe(app.plugins.plumber(
-			app.plugins.notify.onError({
-				title: "ZIP",
-				message: "Error: <%= error.message %>"
-			}))
-		)
-		.pipe(zipPlugin(`${app.path.rootFolder}.zip`))
-		.pipe(app.gulp.dest('./'));
+export const zip = async () => {
+  await deleteAsync(`./${app.path.rootFolder}.zip`);
+  return app.gulp.src(`${app.path.buildFolder}/**/*.*`, {})
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: "ZIP",
+        message: "Error: <%= error.message %>"
+      }))
+    )
+    .pipe(zipPlugin(`${app.path.rootFolder}.zip`))
+    .pipe(app.gulp.dest('./'));
 }
